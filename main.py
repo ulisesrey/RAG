42adbe30d9abfccc5c7a3f01263b4a2fa118976f
ulisesrey <ulises.rey.torne@gmail.com>
2025-07-23 08:56:05 +0200

    refractor

--- main.py
+++ main.py
@@ -22,58 +22,72 @@ logging.basicConfig(
     ]
 )
 
-# Start timer
-start = time.time()
+def batch_vectorize_pdf(path, chunk_size, chunk_overlap, embeddings, batch_size=16):
+    """
+    Vectorize the input pdf
+    """
 
-# Load the data
-pdf_path = "data/charles_darwin_origin_of_species_short.pdf"
-loader = PyPDFLoader(pdf_path)
-documents = loader.load()
+    # Load the data
+    loader = PyPDFLoader(path)
+    documents = loader.load()
 
-# Split the documents
-text_splitter= CharacterTextSplitter(
-    chunk_size=2000, chunk_overlap=10, separator="\n")
+    # Split the documents
+    text_splitter= CharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap, separator="\n")
 
-split_documents = text_splitter.split_documents(documents=documents)
+    split_documents = text_splitter.split_documents(documents=documents)
 
-embeddings = OllamaEmbeddings(model="mistral")
+    # embeddings = OllamaEmbeddings(model="mistral")
 
-# Batch embbedding
-batch_size = 16
-texts = []
-vectors = []
-metadatas = []
+    # Batch embbedding
+    batch_size = batch_size
+    texts = []
+    vectors = []
+    metadatas = []
 
-for i in tqdm(range(0, len(split_documents), batch_size), desc="Embedding batches"):
-    batch_docs = split_documents[i:i + batch_size]
-    batch_texts = [doc.page_content for doc in batch_docs]
-    batch_metas = [doc.metadata for doc in batch_docs]
+    for i in tqdm(range(0, len(split_documents), batch_size), desc="Embedding batches"):
+        batch_docs = split_documents[i:i + batch_size]
+        batch_texts = [doc.page_content for doc in batch_docs]
+        batch_metas = [doc.metadata for doc in batch_docs]
 
-    batch_vectors = embeddings.embed_documents(batch_texts)
+        batch_vectors = embeddings.embed_documents(batch_texts)
 
-    texts.extend(batch_texts)
-    vectors.extend(batch_vectors)
-    metadatas.extend(batch_metas)
+        texts.extend(batch_texts)
+        vectors.extend(batch_vectors)
+        metadatas.extend(batch_metas)
+    
+    return texts, vectors, metadatas
 
-logging.info(f"Created vector store in {time.time() - start:.2f} seconds.")
+
+def save_vectors(texts, vectors, metadatas, embeddings, persist_directory):
+    """
+    Save the texts, vectors and medatada with Chroma
+    """
+    # Vector Store
+    # vector_store = InMemoryVectorStore(embeddings) For in memory (can't be saved)
+    db = Chroma(
+        embedding_function=embeddings,
+        persist_directory=persist_directory
+    )
+    # Dont need db.persist() to save
+    db.add_texts(texts=texts, metadatas=metadatas, embeddings=vectors)
 
 
-# Vector Store
-# vector_store = InMemoryVectorStore(embeddings) For in memory (can't be saved)
-db = Chroma(
-    embedding_function=embeddings,
+
+if __name__ == "__main__":
+
+    # Start timer
+    start = time.time()
+
+
+    path = "data/charles_darwin_origin_of_species_short.pdf"
+    chunk_size, chunk_overlap = 2000, 100
+    batch_size=16  
+    embeddings = OllamaEmbeddings(model="mistral")
+    texts, vectors, metadatas = batch_vectorize_pdf(path, chunk_size, chunk_overlap, embeddings, batch_size)
+    logging.info(f"Created vector store in {time.time() - start:.2f} seconds.")
+
     persist_directory="vector_store_short"
-)
+    save_vectors(texts, vectors, metadatas, embeddings, persist_directory)
 
-# Dont need db.persist() to save
-db.add_texts(texts=texts, metadatas=metadatas, embeddings=vectors)
-
-
-
-logging.info(f"Created and saved Chroma vector store in {time.time() - start:.2f} seconds.")
-
-
-
-
-# if __name__ == "__main__":
-#     main()
+    logging.info(f"Created and saved Chroma vector store in {time.time() - start:.2f} seconds.")
