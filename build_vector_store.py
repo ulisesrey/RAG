de0058c15d5035dcce2e4b252431dda70202ab5f
ulisesrey <ulises.rey.torne@gmail.com>
2025-07-29 14:15:26 +0200

    clean up

--- build_vector_store.py
+++ build_vector_store.py
@@ -8,11 +8,6 @@ from dotenv import load_dotenv
 from utils.vector_store import batch_vectorize_pdf, save_text, save_vectors
 from utils.utils import load_config
 
-load_dotenv()
-
-# Load parameters from config.yaml
-config = load_config()
-
 # Set up logging
 log_id = datetime.now().strftime("%Y%m%d_%H%M%S")
 log_filename = f"logs/{log_id}.log"
@@ -23,14 +18,19 @@ logging.basicConfig(
     handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
 )
 
+def main():
+    # load env variables
+    load_dotenv()
 
-
-if __name__ == "__main__":
+    # Load parameters from config.yaml
+    config = load_config()
     # Start timer
     start = time.time()
 
+    # Load variables from config
     path = config["pdf_path"]
     persist_directory = config["persist_directory"]
+    texts_filename = config["texts_filename"]
     chunk_size, chunk_overlap = config["chunk"]["size"], config["chunk"]["overlap"]
     batch_size = config["batch_size"]
     embeddings = OllamaEmbeddings(model=config["embedding_model"])
@@ -42,10 +42,14 @@ if __name__ == "__main__":
     logging.info(f"Created vector store in {time.time() - start:.2f} seconds...")
     logging.info(f"...Now proceeding to save it in {persist_directory}...")
 
-    save_text(texts, filename="vector_stores/texts_long.txt")
+    save_text(texts, filename=texts_filename)
 
-    db = save_vectors(texts, vectors, metadatas, embeddings, persist_directory)
+    save_vectors(texts, vectors, metadatas, embeddings, persist_directory)
 
     logging.info(
         f"Saved Chroma vector store in {time.time() - start:.2f} seconds."
     )
+
+if __name__ == "__main__":
+    main()
+
